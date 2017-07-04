b34a591f0a63b718e86de32c3a8b7d14080595b6
= <sboychen@PCTE23778.cern.ch>
2017-07-04 16:54:14 +0200

    Initial commit.

---
+++ desktop/src/xyz/codingdaddy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package xyz.codingdaddy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import xyz.codingdaddy.SpellBarExampleMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpellBarExampleMain(), config);
+	}
+}

2cf95a0e0649eb2d84c45d329d22c00af489d0c2
= <sboychen@PCTE23778.cern.ch>
2017-09-14 16:24:06 +0200

    Removed unused imports.

--- core/src/xyz/codingdaddy/SpellBarExampleMain.java
+++ core/src/xyz/codingdaddy/SpellBarExampleMain.java
@@ -9,7 +9,6 @@ import xyz.codingdaddy.hud.SpellBar;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
--- core/src/xyz/codingdaddy/hud/SpellBar.java
+++ core/src/xyz/codingdaddy/hud/SpellBar.java
@@ -3,7 +3,6 @@ package xyz.codingdaddy.hud;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import xyz.codingdaddy.Constants;
 import xyz.codingdaddy.GameTime;
